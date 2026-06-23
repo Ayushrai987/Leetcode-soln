eff73701dadfbf1f9b086dd56199bfd5f8a71d3e
Ayush rai <umeshrai3829@outlook.com>
2026-06-23 23:00:45 +0530

    Refactor canMakeArithmeticProgression to use sorting
    Refactored canMakeArithmeticProgression method to use sorting for checking arithmetic progression.

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,4 +1,34 @@
-import java.util.*;
+1. import java.util.Arrays;
+
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        int n = arr.length;
+
+        // Step 1: sort array
+        Arrays.sort(arr);
+
+        // Step 2: find common difference
+        int d = arr[1] - arr[0];
+
+        // Step 3: check all consecutive differences
+        for (int i = 2; i < n; i++) {
+            if (arr[i] - arr[i - 1] != d) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
+2. import java.util.*;
 
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
