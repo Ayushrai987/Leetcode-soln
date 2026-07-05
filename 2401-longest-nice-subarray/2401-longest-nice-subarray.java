0ac1a25bdd3d50bb0c2d22ea94e10fab6e704560
Ayush rai <umeshrai3829@outlook.com>
2026-07-05 09:31:03 +0530

    Time: 3 ms (99.29%), Space: 81.4 MB (39.11%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int left = 0;
+        int current_party = 0;
+        int maxLength = 0;
+        
+        // right pointer se array ke har number par jayenge
+        for (int right = 0; right < nums.length; right++) {
+            
+            // 1. CLASH CHECK: Agar naye number ka bit aur purane kisi number ka bit match ho gaya
+            while ((current_party & nums[right]) != 0) {
+                // Toh left waale number ko party se nikalo
+                // ^ (XOR) operator se hum us number ke bits ko remove kar dete hain
+                current_party ^= nums[left];
+                left++; // Window ko aage badhao
+            }
+            
+            // 2. ADD TO PARTY: Naye number ko party mein shaamil karo
+            // | (OR) operator se naye number ke bits current_party mein jud jaate hain
+            current_party |= nums[right];
+            
+            // 3. MAX LENGTH UPDATE: Abhi tak ki sabse badi window track karo
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
+    }
+}
