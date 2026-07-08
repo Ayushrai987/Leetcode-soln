8dc6bb2131d153ac695759f02369600e6bf8e61a
Ayush rai <umeshrai3829@outlook.com>
2026-07-08 22:46:15 +0530

    Time: 0 ms (100%), Space: 42 MB (51.88%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,21 @@
-// Pure Recursion Approach for Fibonacci
-// Problem: LeetCode 509 - Fibonacci Number
-
 class Solution {
     public int fib(int n) {
-        // Base cases
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        // Recursive relation
-        return fib(n - 1) + fib(n - 2);
+        //if(n<=0)    
+           // return 0;
+        //if(n==1)
+          //  return 1;
+     //   return fib(n-1)+fib(n-2);*/
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        int[] dp =new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+    return dp[n];
     }
 }
