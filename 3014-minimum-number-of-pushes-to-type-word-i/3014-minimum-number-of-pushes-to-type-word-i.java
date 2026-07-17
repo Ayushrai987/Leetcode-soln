67cb1a4c1d8f68e419b1f416da7041848b9c32a7
Ayush rai <umeshrai3829@outlook.com>
2026-07-17 10:53:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (90.12%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,24 +1,12 @@
-public class Solution {
+class Solution {
     public int minimumPushes(String word) {
-        if (word.length() <= 8) {
-            return word.length();
+        int n = word.length();
+        int pushes = 0;
+
+        for (int i = 0; i < n; i++) {
+            pushes += i / 8 + 1;
         }
 
-        int count = 0;
-	//You can use an array of size 10 also and use index 2 to 9
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        int assign = 2;
-        for (char ch : word.toCharArray()) {
-            if (assign > 9) {
-                assign = 2;
-            }
-
-            mp.put(assign, mp.getOrDefault(assign, 0) + 1);
-            count += mp.get(assign);
-            assign++;
-        }
-
-        return count;
+        return pushes;
     }
 }
